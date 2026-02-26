e1f1b280af18faf6f6c6a0cb77c0db697ff725e7
jhimarcarlmotea <jmotea.a12241914@umak.edu.ph>
2026-02-26 11:38:46 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Outlet } from 'react-router-dom'  // <- Add Outlet
 import { Header, Footer } from './components/Layout'
 import { CartProvider } from "./context/CartContext";
 
 import Jem8HomePage from './Jem8HomePage'
-import About from './pages/About'
 import Blog from './pages/Blog'
 import Products from "./pages/Products";
 import ProductView from "./pages/ProductView";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
 import MyOrders from "./pages/MyOrders";
+import AdminProducts from "./pages/adminProducts";
 
-export default function App() {
+// Layout component for public pages
+function PublicLayout() {  // <- Remove 'children' parameter
   return (
     <>
       <Header />
-      <CartProvider>
-        <Routes>
+      <Outlet />  {/* <- Add Outlet here - ito ang magre-render ng child routes */}
+      <Footer />
+    </>
+  );
+}
+
+export default function App() {
+  return (
+    <CartProvider>
+      <Routes>
+        {/* Public routes with layout */}
+        <Route element={<PublicLayout />}>
           <Route path="/" element={<Jem8HomePage />} />
-          <Route path="/about" element={<About />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/products" element={<Products />} />
           <Route path="/products/:id" element={<ProductView />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/orders" element={<MyOrders />} />
-        </Routes>
-      </CartProvider>
-      <Footer />
-    </>
+        </Route>
+        
+        {/* Admin route - no layout */}
+        <Route path="/adminProducts" element={<AdminProducts />} />
+      </Routes>
+    </CartProvider>
   )
 }
